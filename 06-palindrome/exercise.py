a4486f3b66a9df952ab5be7e71c93ef8984c3a34
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-03-09 11:29:24 +0100

    ok

---
+++ 06-palindrome/exercise.py
@@ -0,0 +1,13 @@
+#/#! /urs/bin/env python
+import random
+
+if __name__ == '__main__':
+    firstWord = str(raw_input('Give me a first word: '))
+    firstWord = firstWord.replace(" ","")
+
+    firstWordBackword = firstWord[::-1]
+
+    if firstWord == firstWordBackword:
+        print("The word %s is a palindrome! (%s)"%(firstWord, firstWordBackword))
+    else:
+        print("The word %s is not a palindrome! (%s)"%(firstWord, firstWordBackword))
