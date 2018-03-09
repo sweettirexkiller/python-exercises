dc09ed4dd4d5b4f87523e5993ebe03fb414cd59e
sweettirexkiller <piotrjankiewicz02@gmail.com>
2018-03-09 14:01:42 +0100

    ok

--- 11-check-primality-functions/exercise.py
+++ 11-check-primality-functions/exercise.py
@@ -1,7 +1,5 @@
 #! /urs/bin/env python
-
-if __name__ == '__main__':
-    number = int(raw_input('Give me a number: '))
+def isPrimary(number):
     list = range(2,number/2 + 1)
     primary = True
     for el in list:
@@ -13,4 +11,9 @@ if __name__ == '__main__':
         print('%s is a primary number!'%number)
     else:
         print('%s is not a primary number!'%number)
+
+if __name__ == '__main__':
+    number = int(raw_input('Give me a number: '))
+    isPrimary(number)
+
 
